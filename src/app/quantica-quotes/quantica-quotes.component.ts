f674a369b65acaa1f7e0af94816fbe142ae47aee
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-22 23:09:21 +0300

    make changes

--- src/app/quantica-quotes/quantica-quotes.component.ts
+++ src/app/quantica-quotes/quantica-quotes.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-quantica-quotes',
-  templateUrl: './quantica-quotes.component.html',
-  styleUrls: ['./quantica-quotes.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class QuanticaQuotesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class AppComponent {
 
 }
