925f6109caa512e43825c1e56906886006b19118
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-22 22:57:39 +0300

    nest the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,10 @@ export class AppComponent {
   quotes:QuanticaQuotes[]=[
     new QuanticaQuotes(1,'Kamotho','Endurance','When life gives you lemons make lemonades.','Samuel Njiru', new Date(2021,8,22),0,0),
     new QuanticaQuotes(2, 'Lucy','life-skill','There is no coming to consciousness.','Mwai Kibaki' ,new Date(2021,8,22),0,0),
-    new QuanticaQuotes(3,'Faith','Philosophy','Who looks outside,dreams; who looks inside,awakes','Carl Jung'),new Date(2021,8,22),0,0),
-    new QuanticaQuotes(4,'Kamau','Mathematics','Mathematics is the queen of the science','Ga') new Date(2021,8,22)
+    new QuanticaQuotes(3,'Faith','Philosophy','Who looks outside,dreams; who looks inside,awakes','Carl Jung',new Date(2021,8,22),0,0),
+    new QuanticaQuotes(4,'Kamau','Mathematics','Mathematics is the queen of the science','Gauss Johnna) new Date(2021,8,22),
+    new QuanticaQuotes(5,'Kim','Philosophy','Don't grieve.Anything you lose comes round in another form',Mario Antoinette')new Date(2021,8,22),0,0
+    new QuanticaQuotes(6,Antony,'Wisdom','I have seen all i have heard all i have forgotten all','Kamotho Njiru', new Date(2021,8,22),0,0),
   ]
   
   
